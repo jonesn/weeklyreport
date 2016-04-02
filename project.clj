251996fb520a86dcbe282941abac627b51466235
Nick Jones <nick.jones@arachnid.co.nz>
2016-04-02 21:27:54 +1300

    Formatting.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure    "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.cli "0.3.3"]
                  [endophile "0.1.2"]
                  [clj-pdf "2.2.0"]
