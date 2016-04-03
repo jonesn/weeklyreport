ea23fc0ded7868163e765675482636e883630a8e
Nick Jones <nick.jones@arachnid.co.nz>
2016-04-03 19:09:36 +1200

    Updated project to 1.0 release.

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
-(defproject weeklyreport "0.1.0-SNAPSHOT"
+(defproject weeklyreport "1.0.0"
   :description "Utility to parse and prepare my weekly update pdf from a markdown file."
-  :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
